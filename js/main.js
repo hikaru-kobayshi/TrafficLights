1d08b37c29c221c7f9371efcd865a89c629846e0
小林　輝 <47589326+hikaru-kobayshi@users.noreply.github.com>
2019-02-25 11:14:06 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 $(function () {
-    $('#button').click(function() {
-      setTimeout(function(){
-        $('#blue').css('background', 'royalblue');
-      },1000);
-      setTimeout(function(){
-        $('#blue').css('background', 'grey');
-      },2000);
-      setTimeout(function() {
-        $('#yellow').css('background', '#FFFF00');
-      },2000);
-      setTimeout(function() {
-        $('#yellow').css('background', 'grey');
-      },3000);
-      setTimeout(function() {
-        $('#red').css('background', '#FF0000');
-      },3000);
-      setTimeout(function() {
-        $('#red').css('background', 'grey');
-      },4000);
-    });
+  $('#button').click(function() {
+    setTimeout(function(){
+      $('#blue').css('background', 'royalblue');
+    },1000);
+    setTimeout(function(){
+      $('#blue').css('background', 'grey');
+    },2000);
+    setTimeout(function() {
+      $('#yellow').css('background', '#FFFF00');
+    },2000);
+    setTimeout(function() {
+      $('#yellow').css('background', 'grey')
+    },3000);
+    setTimeout(function() {
+      $('#red').css('background', '#FF0000');
+    },3000);
+    setTimeout(function() 
+      $('#red').css('background', 'grey');
+    },4000);
+  });
 });
