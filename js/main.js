9e1759a3827beeec5544d8d102432c4214119f04
小林　輝 <47589326+hikaru-kobayshi@users.noreply.github.com>
2019-03-04 11:39:44 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,38 +1,26 @@
 $(function(){
-  const myDefer = $.Deferred();
-  $('#button').click(function(){
-    myDefer.resolve();
-  });
-  $('#button').dblclick(function(){
-    myDefer.reject();
-  });
-
   const duration = 1500;
-  const myPromise = myDefer.promise();
-  myPromise.then(
-    function(){
-      $('#body').delay(duration).queue(function(next) {
-        $(this).attr('light', 'blue').dequeue();
-      }).delay(duration).queue(function(next) {
-        $(this).attr('light', 'yellow').dequeue();
-      }).delay(duration).queue(function(next) {
-        $(this).attr('light', 'red').dequeue();
-      }).delay(duration).queue(function(next) {
-        $(this).attr('light', '').dequeue();
-      });
+  let isRunning = false;
+
+  $('#button').click(function(){
+    if (isRunning) return;
+    isRunning = true;
+    $('#body').delay(duration).queue(function() {
+      $(this).attr('light', 'blue').dequeue();
+    }).delay(duration).queue(function() {
+      $(this).attr('light', 'yellow').dequeue();
+    }).delay(duration).queue(function() {
+      $(this).attr('light', 'red').dequeue();
+    }).delay(duration).queue(function() {
+      $(this).attr('light', '').dequeue();
+      isRunning = false;
     });
-  $('#reload').click(function() {
-    location.reload();
   });
-
-  {
-    const btn = document.getElementById('button');
-
-    btn.addEventListener('mousedown', () => {
-      btn.classList.add('press');
-    });
-    btn.addEventListener('mouseup', () => {
-      btn.classList.remove('press');
-    });
-  }
+  $('#button')
+  .mousedown(function(){
+    $(this).addClass('press');
+  })
+  .mouseup(function(){
+    $(this).removeClass('press');
+  });
 });
