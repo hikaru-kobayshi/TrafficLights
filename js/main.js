7587711c69ecc3c38d8f6b281fdc3f11786a968a
小林　輝 <47589326+hikaru-kobayshi@users.noreply.github.com>
2019-03-15 21:40:37 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,38 @@
-$(function(){
-  const duration = 1500;
+$(() => {
+  'use strict';
+
+  const duration = 1000;
   const colors = ['blue','yellow','red',''];
   const len = colors.length;
   let isRunning = false;
 
-  $('#button').click(function(){
-    if (isRunning)return;
-    isRunning = true;
+  // 信号機の色を時間差で変化させる
+  const transitColor = color =>
+  new Promise(resolve => {
     for (let i=0; i<len; i++) {
-      $.when(
-        $('#wait').addClass('show'),
-        $('#push').addClass('hide'),
-        $('#body').delay(duration).queue(function() {
-          $(this).attr('light', colors[i]).dequeue();
-        })
-      ).done(function() {
-        $('#wait').removeClass('show')
-        $('#push').removeClass('hide')
-        isRunning = false;
+      $('#body').delay(duration).queue(function() {
+        $(this).attr('light', colors[i]).dequeue();
       });
     }
   });
+  // ボタンクリック時の処理およびボタン上の表示切り替え処理
+  $('#button').click(async () => {
+    if (isRunning) return;
+    isRunning = true;
+    for (const color of colors) {
+      console.log("start!");
+      setTimeout(function() {
+        $('#wait').removeClass('show');
+        $('#push').removeClass('hide');
+        console.log("end!");
+      },4000);
+      $('#wait').addClass('show');
+      $('#push').addClass('hide');
+      await transitColor(color);
+      isRunning = false;
+    }
+  });
+  // ボタンが押されたようにするレイアウト
   $('#button')
     .mousedown(function(){
       $(this).addClass('press');
